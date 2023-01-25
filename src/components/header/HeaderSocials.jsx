c6c61bcc7edec0414dcc4b6499d1665efb29dd7b
= <=>
2023-01-25 15:22:18 -0800

    warnings

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,9 +4,9 @@ import { FaGithub } from "react-icons/fa";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com/in/bradvidal" target="_blank"><BsLinkedin/></a>
-      <a href="https://github.com/bvfusion1001" target="_blank"><FaGithub/></a>
-      <a href="https://youtube.com/whereisbrad" target="_blank"><BsYoutube/></a>
+      <a href="https://linkedin.com/in/bradvidal" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+      <a href="https://github.com/bvfusion1001" target="_blank" rel="noreferrer"><FaGithub/></a>
+      <a href="https://youtube.com/whereisbrad" target="_blank" rel="noreferrer"><BsYoutube/></a>
     </div>
   );
 };
