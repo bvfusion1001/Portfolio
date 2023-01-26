744066cb6eaf37f5b3128c8435d124539020f014
= <=>
2023-01-26 01:35:03 -0800

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ import {IoLogoTwitter} from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Brad</a>
+      <a href="#" className='footer__logo'>Brad Vidal</a>
 
       <ul className="permalinks">
         <li><a href="#">Home</a></li>
