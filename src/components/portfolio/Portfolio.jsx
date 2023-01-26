f08bf1909661db89caa2984095afa5f920622c84
= <=>
2023-01-26 01:33:07 -0800

    re-add everythine else

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,43 +1,43 @@
-// import "./portfolio.css";
-// import portfolioItems from "./portfolioItems";
+import "./portfolio.css";
+import portfolioItems from "./portfolioItems";
 
 const Portfolio = () => {
-  // const data = portfolioItems()
+  const data = portfolioItems()
   return (
-    <div id='portfolio'>Portfolio</div>
-    // <section id="portfolio">
-    //   <h5>My Recent Work</h5>
-    //   <h2>Portfolio</h2>
+    // <div id='portfolio'>Portfolio</div>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-    //   <div className="container portfolio__container">
-    //     {data.map(({id, image, title, github, demo}) => {
-    //       return <article key={id} className="portfolio__item">
-    //         <div className="portfolio__item-image">
-    //           <img src={image} alt={title} />
-    //         </div>
-    //         <h3>{title}</h3>
-    //         <div className="portfolio__item-cta">
-    //           <a
-    //             href={github}
-    //             className="btn"
-    //             target="_blank"
-    //             rel="noreferrer"
-    //           >
-    //             Github
-    //           </a>
-    //           <a
-    //             href={demo}
-    //             className="btn btn-primary"
-    //             target="_blank"
-    //             rel="noreferrer"
-    //           >
-    //             Live Demo
-    //           </a>
-    //         </div>
-    //       </article>
-    //     })}
-    //   </div>
-    // </section>
+      <div className="container portfolio__container">
+        {data.map(({id, image, title, github, demo}) => {
+          return <article key={id} className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+              <a
+                href={github}
+                className="btn"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+              <a
+                href={demo}
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+        })}
+      </div>
+    </section>
   );
 };
 export default Portfolio;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,68 +1,67 @@
-// import "./testimonials.css";
-// import AVTR1 from "../../assets/avatar1.jpg";
-// import AVTR2 from "../../assets/avatar2.jpg";
-// import AVTR3 from "../../assets/avatar3.jpg";
-// import AVTR4 from "../../assets/avatar4.jpg";
+import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
 
 // Swiper
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import { Pagination } from "swiper";
-// import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-// const data = [
-//   {
-//     avatar: AVTR1,
-//     name: "Tina Snow",
-//     review:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum enim laboriosam quisquam. Aperiam temporibus odit a labore, cumque nulla voluptatem? Nesciunt quod culpa consectetur qui a iste provident facere ducimus!",
-//   },
-//   {
-//     avatar: AVTR2,
-//     name: "Shatta Wale",
-//     review:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sit, veniam eaque odio laboriosam, id quibusdam quam, harum soluta similique ratione facilis repudiandae neque voluptatibus possimus eum cum voluptatem temporibus!",
-//   },
-//   {
-//     avatar: AVTR3,
-//     name: "Kwame Despite",
-//     review:
-//       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt porro illo nobis quaerat odio mollitia obcaecati necessitatibus minima asperiores ducimus dolorem officia iure tenetur magnam praesentium, ut cum nihil architecto?",
-//   },
-//   {
-//     avatar: AVTR4,
-//     name: "Nana Ama McBrown",
-//     review:
-//       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus dolore cupiditate modi incidunt aperiam fugiat, soluta quas dolores perspiciatis hic omnis ea impedit reprehenderit ut aut blanditiis numquam, sit asperiores.",
-//   },
-// ];
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Tina Snow",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum enim laboriosam quisquam. Aperiam temporibus odit a labore, cumque nulla voluptatem? Nesciunt quod culpa consectetur qui a iste provident facere ducimus!",
+  },
+  {
+    avatar: AVTR2,
+    name: "Shatta Wale",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sit, veniam eaque odio laboriosam, id quibusdam quam, harum soluta similique ratione facilis repudiandae neque voluptatibus possimus eum cum voluptatem temporibus!",
+  },
+  {
+    avatar: AVTR3,
+    name: "Kwame Despite",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt porro illo nobis quaerat odio mollitia obcaecati necessitatibus minima asperiores ducimus dolorem officia iure tenetur magnam praesentium, ut cum nihil architecto?",
+  },
+  {
+    avatar: AVTR4,
+    name: "Nana Ama McBrown",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus dolore cupiditate modi incidunt aperiam fugiat, soluta quas dolores perspiciatis hic omnis ea impedit reprehenderit ut aut blanditiis numquam, sit asperiores.",
+  },
+];
 
 const Testimonials = () => {
   return (
-    <div id="testimonials">Testimonials</div>
-    // <section id="testimonials">
-    //   <h5>Review from clients</h5>
-    //   <h2>Testimonials</h2>
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
 
-    //   <Swiper className="container testimonials__container"
-    //   modules={[Pagination]}
-    //   spaceBetween={40}
-    //   slidesPerView={1}
-    //   pagination={{clickable: true}}
-    //   >
-    //     {data.map(({ avatar, name, review }, index) => {
-    //       return (
-    //         <SwiperSlide key={index} className="testimonial">
-    //           <div className="client__avatar">
-    //             <img src={avatar} alt={name} />
-    //           </div>
-    //           <h5 className="client__name">{name}</h5>
-    //           <small className="client__review">{review}</small>
-    //         </SwiperSlide>
-    //       );
-    //     })}
-    //   </Swiper>
-    // </section>
+      <Swiper className="container testimonials__container"
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{clickable: true}}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
   );
 };
 export default Testimonials;
