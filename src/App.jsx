264dff686c16b2b33d9e5410c01a121a0c42d5c4
= <=>
2023-01-26 00:26:23 -0800

    Footer, deploy (homepage)

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
+// import About from './components/about/About'
+// import Experience from './components/experience/Experience'
 // import Services from './components/services/Services'
 // import Portfolio from './components/portfolio/Portfolio'
 // import Testimonials from './components/testimonials/Testimonials'
-import Contact from './components/contact/Contact'
-// import Footer from './components/footer/Footer'
+// import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 
 const App = () => {
   return (
     <>
-    <div>This is an app</div>
-      {/* <Header/>
+    {/* <div>This is an app</div> */}
+      <Header/>
       <Nav/>
-      <About/>
+      {/* <About/>
       <Experience/>
       <Services/>
       <Portfolio/>
       <Testimonials/>
-      <Contact/>
-      <Footer/> */}
+      <Contact/> */}
+      <Footer/>
     </>
   )
 }
