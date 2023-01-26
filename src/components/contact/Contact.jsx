cf4495abf449d637f3b8df62484b5f46f71aa6ad
= <=>
2023-01-25 17:27:17 -0800

    Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,55 @@
-import './contact.css'
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+
+const email = "bradvidal@gmail.com";
+const facebookId = "brad.vidal";
+const phone = "+1(909)240-1143";
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
-  )
-}
-export default Contact
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>{email}</h5>
+            <a href={`mailto:${email}`}>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>{facebookId}</h5>
+            <a href={`https://m.me/${facebookId}`}>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>Whatsapp</h4>
+            <h5>{phone}</h5>
+            <a href={`https://api.whatsapp.com/send?phone=${phone}`}>
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* END OF */}
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="text" name="email" placeholder="Email" required />
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type='submit' className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+export default Contact;
