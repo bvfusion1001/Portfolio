4ff3b9870b955acfb803f607c306f74d6e3f9d3e
= <=>
2023-01-26 01:29:02 -0800

    re-add everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
-// import About from './components/about/About'
-// import Experience from './components/experience/Experience'
-// import Services from './components/services/Services'
-// import Portfolio from './components/portfolio/Portfolio'
-// import Testimonials from './components/testimonials/Testimonials'
-// import Contact from './components/contact/Contact'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
 
@@ -14,13 +14,13 @@ const App = () => {
     <>
       <Header/>
       <Nav/>
-      {/* <About/>
+      <About/>
       <Experience/>
       <Services/>
       <Portfolio/>
       <Testimonials/>
-      <Contact/> */}
-      {/* <Footer/> */}
+      <Contact/>
+      <Footer/>
     </>
   )
 }
