4583dc3f2778bf9d2207e2a04e2765aa7f7800e3
= <=>
2023-01-25 16:23:37 -0800

    comment Services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,99 +3,100 @@ import "./services.css";
 
 const Services = () => {
   return (
-    <section id="services">
-      <h5>What I Offer</h5>
-      <h2>Services</h2>
+    <div id='services'>Services</div>
+    // <section id="services">
+    //   <h5>What I Offer</h5>
+    //   <h2>Services</h2>
 
-      <div className="container services__container">
-        {/* UI/UX */}
-        <article className="service">
-          <div className="service__head">
-            <h3>UI/UX Design</h3>
-          </div>
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-          </ul>
-        </article>
+    //   <div className="container services__container">
+    //     {/* UI/UX */}
+    //     <article className="service">
+    //       <div className="service__head">
+    //         <h3>UI/UX Design</h3>
+    //       </div>
+    //       <ul className="service__list">
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //       </ul>
+    //     </article>
 
-        {/* WEB DEVELOPMENT */}
-        <article className="service">
-          <div className="service__head">
-            <h3>UI/UX Design</h3>
-          </div>
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-          </ul>
-        </article>
+    //     {/* WEB DEVELOPMENT */}
+    //     <article className="service">
+    //       <div className="service__head">
+    //         <h3>UI/UX Design</h3>
+    //       </div>
+    //       <ul className="service__list">
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //       </ul>
+    //     </article>
 
-        {/* CONTENT CREATION */}
-        <article className="service">
-          <div className="service__head">
-            <h3>UI/UX Design</h3>
-          </div>
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-          </ul>
-        </article>
-      </div>
-    </section>
+    //     {/* CONTENT CREATION */}
+    //     <article className="service">
+    //       <div className="service__head">
+    //         <h3>UI/UX Design</h3>
+    //       </div>
+    //       <ul className="service__list">
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //         <li>
+    //           <BiCheck className="service__list-icon" />
+    //           <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+    //         </li>
+    //       </ul>
+    //     </article>
+    //   </div>
+    // </section>
   );
 };
 export default Services;
