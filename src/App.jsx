381fdfbfa88f936c2ae9622520538e42841406e0
= <=>
2023-01-26 00:04:48 -0800

    Super simplify

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import Experience from './components/experience/Experience'
 // import Services from './components/services/Services'
 // import Portfolio from './components/portfolio/Portfolio'
 // import Testimonials from './components/testimonials/Testimonials'
-// import Contact from './components/contact/Contact'
+import Contact from './components/contact/Contact'
 // import Footer from './components/footer/Footer'
 
 
 const App = () => {
   return (
     <>
-      <Header/>
+    <div>This is an app</div>
+      {/* <Header/>
       <Nav/>
       <About/>
       <Experience/>
-      {/* <Services/>
+      <Services/>
       <Portfolio/>
       <Testimonials/>
       <Contact/>
